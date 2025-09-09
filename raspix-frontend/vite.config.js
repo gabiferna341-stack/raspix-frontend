1adeaa5d975d3a20c964744c78625ad73044432b
gabiferna341-stack <gabiferna341@gmail.com>
2025-09-09 17:17:57 +0000

    fix: vite base config for Render

--- raspix-frontend/vite.config.js
+++ raspix-frontend/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // Configuração do Vite
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 👈 garante que os assets funcionem em produção no Render
   server: {
-    port: 5173, // Porta padrão, mas pode alterar se precisar
-    open: true  // Abre automaticamente no navegador ao rodar `npm run dev`
+    port: 5173,
+    open: true
   },
   resolve: {
     alias: {
-      '@': '/src', // Facilita importações: você pode usar "@/components"
+      '@': '/src',
     },
   },
 })
